8d6ec5c26f75ba6e7aba5c244100c65371fcfcaa
Jeremy Brown <chase208@4JLT.com>
2025-02-05 06:41:26 -0500

    Allowing all origins temporarily

--- src/main/java/com/periodictable/elements/CorsMapping.java
+++ src/main/java/com/periodictable/elements/CorsMapping.java
@@ -13,7 +13,7 @@ public class CorsMapping implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
