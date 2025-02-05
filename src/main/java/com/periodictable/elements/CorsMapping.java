9248dd3870c01447ab781c8a34701916d900bed9
Jeremy Brown <chase208@4JLT.com>
2025-02-04 23:23:16 -0500

    Adding options method

--- src/main/java/com/periodictable/elements/CorsMapping.java
+++ src/main/java/com/periodictable/elements/CorsMapping.java
@@ -14,7 +14,7 @@ public class CorsMapping implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
